e1435554e7ff89ec3780b15922a4130b99bfda5f
Bahram Movlanov <bmovlanov@gmail.com>
2022-05-21 21:46:07 -0600

    finished

---
+++ day_11/script.js
@@ -0,0 +1,9 @@
+const insertEl = document.querySelector('#insert')
+
+window.addEventListener('keydown', (e) => {
+	insertEl.innerHTML = `
+    <div class="key">${e.key === ' ' ? 'Space' : e.key} <small>event.key</small></div>
+    <div class="key">${e.keyCode} <small>event.keyCode</small></div>
+    <div class="key">${e.code} <small>event.code</small></div>
+  `
+})
